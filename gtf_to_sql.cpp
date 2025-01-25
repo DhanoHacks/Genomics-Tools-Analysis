4de76d78db02ebb296449eb01a5242da1614a04b
DhanoHacks <draman9224@gmail.com>
2025-01-25 18:23:14 +0530

    multithreaded c++ code

--- gtf_to_sql.cpp
+++ gtf_to_sql.cpp
@@ -6,6 +6,11 @@
 #include <sstream>
 #include <unordered_map>
 #include <algorithm>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+mutex gtf_mtx, sql_mutex;
 
 std::string processLine(const std::string &line) {
     // Split the line by tabs
@@ -66,33 +71,88 @@ std::string processLine(const std::string &line) {
     return sql;
 }
 
-using namespace std;
-int main(){
-    sqlite3 *db;
-    int rc;
-    rc = sqlite3_open(":memory:", &db);
-    rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS \"human\" (\"index\" INTEGER,\"Chromosome\" TEXT,Source\" TEXT,\"Feature\" TEXT,\"Start\" INTEGER,\"End\" INTEGER,\"Score\" TEXT,\"Strand\" TEXT,\"Frame\" TEXT,\"gene_id\" TEXT,\"gene_version\" TEXT,\"gene_source\" TEXT,\"gene_biotype\" TEXT,\"transcript_id\" TEXT,\"transcript_version\" TEXT,\"transcript_source\" TEXT,\"transcript_biotype\" TEXT,\"tag\" TEXT,\"transcript_support_level\" TEXT,\"exon_number\" TEXT,\"exon_id\" TEXT,\"exon_version\" TEXT,\"gene_name\" TEXT,\"transcript_name\" TEXT,\"protein_id\" TEXT,\"protein_version\" TEXT,\"ccds_id\" TEXT);" , NULL, NULL, NULL);
-    rc = sqlite3_exec(db, "PRAGMA journal_mode = OFF; PRAGMA synchronous = 0; PRAGMA cache_size = 1000000; PRAGMA locking_mode = EXCLUSIVE; PRAGMA temp_store = MEMORY;", NULL, NULL, NULL);
-    // open file "Homo_sapiens.GRCh38.112.chr.gtf"
-    ifstream inputFile("Homo_sapiens.GRCh38.112.chr.gtf");
-    string line;
+void mythread(ifstream *inputFile, sqlite3 **db){
+    string line, linedata="";
+    vector<string> lines;
     int linecount = 0;
-    while (getline(inputFile, line)) {
-        linecount++;
+    bool eof = false;
+    while (!eof) {
+        gtf_mtx.lock();
+        for(int i=0; i<10; i++){
+            if(!getline(*inputFile, line)){
+                eof = true;
+                break;
+            }
+            // ignore first few lines beginning with #
+            if(line[0] == '#'){
+                continue;
+            }
+            lines.push_back(line);
+        }
+        gtf_mtx.unlock();
+
+        linecount+=10;
         if (linecount%100000 == 0){
             cout<<"Processed "<<linecount<<" lines"<<endl;
         }
-        // ignore first few lines beginning with #
-        if(line[0] == '#'){
-            continue;
+
+        for(auto line: lines){
+            linedata += processLine(line);
         }
-        string linedata = processLine(line);
-        // Output the parsed data
-        // vector<string> keys={"Chromosome", "Source", "Feature", "Start", "End", "Score", "Strand", "Frame", "Attribute"};
-        // sqlite3_stmt *ppStmt;
-        // rc = sqlite3_prepare_v2(db,linedata.c_str(),-1,&ppStmt,NULL);
-        // rc = sqlite3_step(ppStmt);
-        rc = sqlite3_exec(db, linedata.c_str(), NULL, NULL, NULL);
+        lines.clear();
+
+        if(linecount%50 == 0){
+            sql_mutex.lock();
+            sqlite3_exec(*db, linedata.c_str(), NULL, NULL, NULL);
+            sql_mutex.unlock();
+            linedata = "";
+        }
     }
+    if(linedata != ""){
+        sql_mutex.lock();
+        sqlite3_exec(*db, linedata.c_str(), NULL, NULL, NULL);
+        sql_mutex.unlock();
+    }
+}
+
+int main(){
+    sqlite3 *db;
+    int rc;
+    rc = sqlite3_open("db2.sqlite3", &db);
+    rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS \"human\" (\"Chromosome\" TEXT, \"Source\" TEXT,\"Feature\" TEXT,\"Start\" INTEGER,\"End\" INTEGER,\"Score\" TEXT,\"Strand\" TEXT,\"Frame\" TEXT,\"gene_id\" TEXT,\"gene_version\" TEXT,\"gene_source\" TEXT,\"gene_biotype\" TEXT,\"transcript_id\" TEXT,\"transcript_version\" TEXT,\"transcript_source\" TEXT,\"transcript_biotype\" TEXT,\"tag\" TEXT,\"transcript_support_level\" TEXT,\"exon_number\" TEXT,\"exon_id\" TEXT,\"exon_version\" TEXT,\"gene_name\" TEXT,\"transcript_name\" TEXT,\"protein_id\" TEXT,\"protein_version\" TEXT,\"ccds_id\" TEXT);" , NULL, NULL, NULL);
+    rc = sqlite3_exec(db, "PRAGMA journal_mode = OFF; PRAGMA synchronous = 0; PRAGMA cache_size = 1000000; PRAGMA locking_mode = EXCLUSIVE; PRAGMA temp_store = MEMORY;", NULL, NULL, NULL);
+    // open file "Homo_sapiens.GRCh38.112.chr.gtf"
+    ifstream inputFile("Homo_sapiens.GRCh38.112.chr.gtf");
+    // string line;
+    // int linecount = 0;
+    // while (getline(inputFile, line)) {
+    //     linecount++;
+    //     if (linecount%100000 == 0){
+    //         cout<<"Processed "<<linecount<<" lines"<<endl;
+    //     }
+    //     // ignore first few lines beginning with #
+    //     if(line[0] == '#'){
+    //         continue;
+    //     }
+    //     // string linedata = processLine(line);
+    //     // rc = sqlite3_exec(db, linedata.c_str(), NULL, NULL, NULL);
+    // }
+    thread t1(mythread, &inputFile, &db);
+    thread t2(mythread, &inputFile, &db);
+    thread t3(mythread, &inputFile, &db);
+    thread t4(mythread, &inputFile, &db);
+    thread t5(mythread, &inputFile, &db);
+    thread t6(mythread, &inputFile, &db);
+    thread t7(mythread, &inputFile, &db);
+    thread t8(mythread, &inputFile, &db);
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    t6.join();
+    t7.join();
+    t8.join();
+    rc = sqlite3_exec(db, "CREATE INDEX \"ix_human_index\"ON \"human\" (\"index\");", NULL, NULL, NULL);
     sqlite3_close(db);
 }
